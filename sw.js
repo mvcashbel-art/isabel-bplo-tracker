930da136ca38a0fa50398665e358414eb73aff54
mvcashbel-art <mvcashbel@gmail.com>
2026-08-25 15:55:39 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'isabel-bplo-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let Google Script/API calls bypass cache; cache local shell
+  if (event.request.url.includes('script.google.com') || event.request.url.includes('nominatim')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
